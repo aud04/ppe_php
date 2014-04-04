711edb12ee20af17482bcbad673fd500dcff06d8
stephan <stephan@stephan-desktop>
2014-04-04 12:16:17 +0200

    Modification de la connection a la base de donnée

--- modele/conn_sql.php
+++ modele/conn_sql.php
@@ -5,10 +5,10 @@ try
     //$bdd = new PDO('mysql:host=mysql51-96.perso;dbname=hbdeveloppeur1', 'hbdeveloppeur1', 'homerique1');
     
      // Ou se connecte à MySQL de mon localhost (wamp)
-     $bdd = new PDO('mysql:host=localhost;dbname=ppejura', 'root', '');
+     //$bdd = new PDO('mysql:host=localhost;dbname=ppejura', 'root', '');
      
      // Ou se connecte à MySQL de mon localhost(linux)
-     // $bdd = new PDO('mysql:host=localhost;dbname=ppeJura', 'root', 'TEST');
+     $bdd = new PDO('mysql:host=localhost;dbname=ppejura', 'root', 'test');
      
 }
 catch(Exception $e)
